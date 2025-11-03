c586dbbedb977c54368c84a650d00a2555178cb1
samuelwildary <samuelwildary@gmail.com>
2025-11-03 10:45:59 -0300

    Delete backend directory

--- backend/schemas/__init__.py
+++
@@ -1,9 +0,0 @@
-from .user import UserCreate, UserResponse, UserLogin, Token
-from .supermarket import SupermarketCreate, SupermarketResponse, SupermarketUpdate
-from .pedido import PedidoCreate, PedidoResponse, ItemPedidoCreate, ItemPedidoResponse
-
-__all__ = [
-    "UserCreate", "UserResponse", "UserLogin", "Token",
-    "SupermarketCreate", "SupermarketResponse", "SupermarketUpdate",
-    "PedidoCreate", "PedidoResponse", "ItemPedidoCreate", "ItemPedidoResponse"
-]
